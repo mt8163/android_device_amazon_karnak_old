c4c0b2e38b15121546187d44274215a7a23842d5
kjones <zaksenoj@icloud.com>
2019-06-04 21:58:02 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #bin/bash
 var=userdebug
-outdir=/home/kai/android/nougat/out/target/product/karnak/
+outdir=$(pwd)/out/target/product/karnak
 echo "**********SETTING UP ENVIROMENT************"
 source build/envsetup.sh
 echo "*******************************************"
 lunch full_karnak-$var
 #you can rename this to your out directory ^
 echo "*************COPYING ROOTDIR***************"
-cp -rv /home/kai/android/nougat/device/amazon/karnak/root $outdir
+cp -rv $(pwd)/device/amazon/karnak/root $outdir/
 echo "*******************************************"
 export LC_ALL=C
 export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
 echo "************STARTING BUILD*****************"
 mka bacon
-echo "************BUILD COMPLETE!****************"
+if [ -f $outdir/*.zip ]
+then
+    echo "************BUILD COMPLETE!****************"
+else 
+    echo "************BUILD FAILED!****************"
+fi
+
+
