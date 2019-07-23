b78b06dc1febb4e32d9007d695870c93d0e97b16
Kai Jones <zaksenoj@icloud.com>
2019-07-23 10:36:48 -0700

    change ui to gui

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,7 +207,7 @@ WITHOUT_CHECK_API := true
 TARGET_LD_SHIM_LIBS := \
 /system/lib/libasp.so|libshim_asp.so \
 /vendor/lib/egl/libGLES_mali.so|libshim_egl.so \
-/vendor/lib/libgui_ext.so|libshim_ui.so \
+/vendor/lib/libgui_ext.so|libshim_gui.so \
 
 
 
--- libshims/Android.mk
+++ libshims/Android.mk
@@ -21,9 +21,9 @@ include $(BUILD_SHARED_LIBRARY)
 
 ## libshim_ui
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ui/mtk_ui.cpp
+LOCAL_SRC_FILES := gui.cpp
 LOCAL_SHARED_LIBRARIES :=libgui
-LOCAL_MODULE := libshim_ui
+LOCAL_MODULE := libshim_gui
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
