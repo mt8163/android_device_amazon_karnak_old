e55a4a97da9b96d0d7c3d6128c637deb408d1db1
Kai Jones <zaksenoj@icloud.com>
2019-07-30 23:10:26 -0700

    remove eng variant adb configs
    * these lines are used for eng build. also for secuity reasons

--- lineage_karnak.mk
+++ lineage_karnak.mk
@@ -22,18 +22,12 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Default.prop
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.adb.secure=0 \
-    ro.secure=0 \
-    ro.debuggable=1 \
     persist.service.acm.enable=0 \
     persist.sys.dun.override=0 \
     camera.disable_zsl_mode=1 \
     sys.usb.ffs.aio_compat=1 \
     ro.mount.fs=EXT4 \
     ro.mtk_key_manager_kb_path=1 \
-    persist.sys.usb.config=mtp,adb \
-    persist.service.adb.enable=1 \
-    persist.service.debuggable=1 \
     persist.sys.debug.multi_window=true
 
 #Build.prop
