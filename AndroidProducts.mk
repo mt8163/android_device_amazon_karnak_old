f632e1882cf65c016c4b5a98446757adc84a1df7
Kai Jones <zaksenoj@icloud.com>
2019-09-05 13:13:20 -0700

    Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Icf0015cb6c5525bcd3f53c2311073b4a8aa2d339

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,1 +1,7 @@
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/lineage_karnak.mk
+
+
+COMMON_LUNCH_CHOICES := \
+     lineage_karnak-eng \
+     lineage_karnak-userdebug \
+     lineage_karnak-user
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo lineage_karnak-userdebug
-add_lunch_combo lineage_karnak-eng
-add_lunch_combo lineage_karnak-user
