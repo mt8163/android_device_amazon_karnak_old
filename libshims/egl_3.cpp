4412c3eecff7e82ef4d1d4f7612714b26cdaad88
Joel Stein <js@funkturm.net>
2019-09-18 05:29:29 +0200

    karnak: fix egl_3 shim

--- libshims/egl_3.cpp
+++ libshims/egl_3.cpp
@@ -2,13 +2,12 @@
 #include <ui/GraphicBufferMapper.h>
 
 using namespace android;
-
 extern "C" status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
 		buffer_handle_t handle, uint32_t usage, const Rect& bounds,
                 void** vaddr, int32_t* outBytesPerPixel,
                 int32_t* outBytesPerStride);
 
-extern "C" status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(
-	buffer_handle_t handle, int32_t usage, const Rect& bounds, void** vaddr) {
-	return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(handle, static_cast<uint32_t>(usage), bounds, vaddr, nullptr, nullptr);
+extern "C" status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
+	buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr) {
+	return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(handle, usage, bounds, vaddr, nullptr, nullptr);
 }
