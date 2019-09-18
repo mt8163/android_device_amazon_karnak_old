3e95426b2c5f770404cb25ec75380e1bc00e8186
Kai Jones <zaksenoj@icloud.com>
2019-09-17 18:11:17 -0700

    Update egl_3.cpp

--- libshims/egl_3.cpp
+++ libshims/egl_3.cpp
@@ -4,8 +4,9 @@
 
 using namespace android;
 
-//status_t GraphicBufferMapper::lock(buffer_handle_t handle, uint32_t usage,
-//       const Rect& bounds, void** vaddr)
+status_t GraphicBufferMapper::lock(buffer_handle_t handle, uint32_t usage, const Rect& bounds,
+                                   void** vaddr, int32_t* outBytesPerPixel,
+                                   int32_t* outBytesPerStride)
 extern "C" void* _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
 	buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr);
 
