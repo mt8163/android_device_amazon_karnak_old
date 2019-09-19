ec370390a147411d2883cb43150a6ee011f4ccf5
Kai Jones <zaksenoj@icloud.com>
2019-09-18 23:01:06 -0700

    Karnak:NVRAM: create fstab symlink
    * this symolic link will fix the error with nvram.
    09-18 04:53:56.238   293   293 D NVRAM   : failed to read /data/nvram/fstab.mt8163
    09-18 04:53:56.241   293   293 D NVRAM   : failed to read fstab
    09-18 04:53:56.241   293   293 D NVRAM   : failed to get partition path
    09-18 04:53:56.241   293   293 D NVRAM   : Total nvram Lid number is -1
    09-18 04:53:56.241   293   293 D NVRAM   : Total nvram Lid number is error !!!

--- Android.mk
+++ Android.mk
@@ -4,4 +4,15 @@ ifeq ($(TARGET_DEVICE),karnak)
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
+
+include $(CLEAR_VARS)
+
+FSTAB_SYMLINK := $(TARGET_ROOT_OUT)/fstab.mt8163
+$(FSTAB_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "fstab link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/etc/fstab.mt8163 $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FSTAB_SYMLINK)
 endif
