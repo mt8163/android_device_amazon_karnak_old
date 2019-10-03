c5ec7f4de37df9bab5e9df0134cf18edb74c85f3
Joel Stein <js@funkturm.net>
2019-10-03 20:00:01 +0200

    karnak: update DRM config

--- hidl.mk
+++ hidl.mk
@@ -26,7 +26,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1-service.clearkey \
-    android.hardware.drm@1.1-service.widevine
 
 
 
