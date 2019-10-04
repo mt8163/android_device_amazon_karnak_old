815cdedd1f597cb7f017e545de05e3691cbe1c15
Kai Jones <zaksenoj@icloud.com>
2019-10-03 20:58:56 -0700

    karnak:hidl: move renderscript hal to HIDL
    Signed-off-by: Kai Jones <zaksenoj@icloud.com>

--- hidl.mk
+++ hidl.mk
@@ -75,6 +75,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl-mediatek \
