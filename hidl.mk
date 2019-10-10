0ca7c5a5279fd80a1ce093b6c97e36b2d6d1c530
Kai <zaksenoj@icloud.com>
2019-10-10 08:32:29 -0700

    karnak: hidl : build sensors from source
    i will remove aosp ones soon

--- hidl.mk
+++ hidl.mk
@@ -66,7 +66,10 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service 
+    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl-mediatek \
+    android.hardware.sensors@1.0-service-mediatek
+
 
 # Power
 PRODUCT_PACKAGES += \
