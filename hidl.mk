dedff9c305ca16fa11baac6ca530804ee3afd380
Kai <zaksenoj@icloud.com>
2019-10-10 08:31:42 -0700

    karnak:hidl: build mediatek bluetooth service and implementation instead of aosp

--- hidl.mk
+++ hidl.mk
@@ -87,8 +87,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-impl-mediatek \
+    android.hardware.bluetooth@1.0-service-mediatek
 
 # Wifi
 PRODUCT_PACKAGES += \
