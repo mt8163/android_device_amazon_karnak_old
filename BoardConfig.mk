f6a6c2e7626c46ce6c7cc28918d0d984f26859c6
Kai Jones <zaksenoj@icloud.com>
2019-12-16 16:15:15 -0800

    karnak:build kernel from source
    Signed-off-by: Kai Jones <zaksenoj@icloud.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,16 +7,11 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 #Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_PREBUILT_KERNEL := device/amazon/karnak/prebuilt/$(BOARD_KERNEL_IMAGE_NAME)
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)prebuilt/zImage
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_KERNEL):kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/amazon/karnak
+TARGET_KERNEL_CONFIG := lineageos_karnak_defconfig
+TARGET_KERNEL_VARIANT_CONFIG :=lineageos_karnak_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX :=  /build/android/nougat/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := karnak
