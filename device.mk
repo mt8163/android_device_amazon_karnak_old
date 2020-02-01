156f3946c5b32d7da8b38ae5bfa3e5ee53f87b7d
Kai Jones <zaksenoj@icloud.com>
2020-02-01 15:10:04 -0800

    karnak:swcodec: add media swcodec configuration
    pulled from t-alps-release-q0 BSP
    
    Change-Id: I2f0b76773fba8292780672ba74555bcd3001296b
    Signed-off-by: Kai Jones <zaksenoj@icloud.com>

--- device.mk
+++ device.mk
@@ -126,7 +126,8 @@ PRODUCT_COPY_FILES += \
 # Seccomp_policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp-policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp-policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp-policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
+    $(LOCAL_PATH)/seccomp-policy/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Wifi
 PRODUCT_COPY_FILES += \
