66db2d8de6ffd411a86d64703c8c6d40def9b8f9
Bruno Martins <bgcngm@gmail.com>
2020-02-08 23:26:24 -0800

    karnak: Uprev mapper to 2.1
    Bug: 66876469
    Test: Enforcement through VTS
    
    Change-Id: I2c2f5a1b7d31f2dd76deff94abc62f9b12a68ffd
    Signed-off-by: Kai Jones <zaksenoj@icloud.com>

--- hidl.mk
+++ hidl.mk
@@ -46,6 +46,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service
 
