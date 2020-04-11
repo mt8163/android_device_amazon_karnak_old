43b7212a45625352177b76546b2675ce9240af06
Kai Jones <joneskai626@gmail.com>
2020-04-11 10:20:37 -0700

    [DNM]karnak:sepolicy:dont use this folder for oreo
    it has pie changes which causes build errors
    
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I7f3638b9fc674dd0e4d05f76417993d3de7832cc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,7 @@ BLOCK_BASED_OTA := true
 
 # Seccomp filters
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp-policy
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
