117c7d0c7e64ae231e093861fdf509bcd482118f
Kai Jones <joneskai626@gmail.com>
2020-05-02 22:16:49 -0700

    [DNM]karnak:selinux:switch back to permissive
    -audio-shim wont load properly so I had to go back to permissive.
    
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I6613ec8b89ff368ce3636776e890e8c81ebd4805

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_HAS_MTK_HARDWARE := true
 
