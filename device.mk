46b98ff6aa445f4b3c0ce445d783f7f717f97c0b
Kai Jones <joneskai626@gmail.com>
2020-06-22 22:29:31 -0700

    karnak:BoardConfig:remove unused DEVICE_FOLDER flag
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I3b7d640353c34bccac6e6c54ee0e6dc28dedc2af

--- device.mk
+++ device.mk
@@ -15,7 +15,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 LOCAL_PATH := device/amazon/karnak
-DEVICE_FOLDER := device/amazon/karnak
 
 # Build Date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
