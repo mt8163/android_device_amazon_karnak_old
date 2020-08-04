b62fa2eb46da481dea2a8965342cd50b7cee286e
Kai Jones <joneskai626@gmail.com>
2020-08-04 00:00:53 -0700

    karnak:switch to full 64 bit support
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I4c7cf27067edf14f82b6c11975f162790e56804d

--- lineage_karnak.mk
+++ lineage_karnak.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, vendor/amazon/mt8163/build/target/product/core_32_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 $(call inherit-product, device/amazon/karnak/device.mk)
