04b2e987423fade79e2a062e1b80d95cd47a586e
Kai Jones <joneskai626@gmail.com>
2020-09-05 12:05:31 -0700

    karnak:arch:this is not 64 bit os anymore
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I0c86839b6e88e21c8b22866744c42e9d24b31098

--- lineage_karnak.mk
+++ lineage_karnak.mk
@@ -16,7 +16,6 @@
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 $(call inherit-product, device/amazon/karnak/device.mk)
