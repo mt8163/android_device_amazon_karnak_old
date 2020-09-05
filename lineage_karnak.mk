5bc753934e6e331275efb3adbc9a09a2dc45d008
faham1997 <nafidfaham08@gmail.com>
2020-09-05 12:04:00 -0700

    karnak: Product was launched with P
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Signed-off-by: faham1997 <nafidfaham08@gmail.com>
    Change-Id: I4f9c336e5a4068540a47052c2cbc134bdfbee961

--- lineage_karnak.mk
+++ lineage_karnak.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
