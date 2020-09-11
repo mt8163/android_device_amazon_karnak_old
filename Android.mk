c2c3dba9c8dcd38b301448407cad8170a94b1068
Kai Jones <joneskai626@gmail.com>
2020-09-11 09:48:21 -0700

    karnak:update nvdata symlink
    nvram uses /data/vendor/nvram
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I6fdcfbd6c6931e992c69a3f2e3479b018d812431

--- Android.mk
+++ Android.mk
@@ -21,10 +21,10 @@ include $(call first-makefiles-under,$(LOCAL_PATH))
 
 NVDATA_SYMLINK := $(TARGET_OUT_VENDOR)/nvdata
 $(NVDATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "fstab link: $@"
+	@echo "nvdata link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /data/nvram $@
+	$(hide) ln -sf /data/vendor/nvram $@
 
 
 ALL_DEFAULT_INSTALLED_MODULES +=  \
