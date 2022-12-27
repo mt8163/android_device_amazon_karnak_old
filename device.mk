af37ca054e502a6477317d87bf449333df66fbb2
Volodymyr Zhdanov <wight554@gmail.com>
2022-12-27 13:12:20 +0100

    karnak: Disable SF configstore * ship disable_configstore because we have API level 29

--- device.mk
+++ device.mk
@@ -176,6 +176,10 @@ PRODUCT_PACKAGES += \
     librs_jni \
     com.android.future.usb.accessory
 
+# Disable SF configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml \
