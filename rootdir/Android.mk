1e1656c0eb0723fd6fe8f5c20d8ffbf42dd0de84
Kai Jones <kvjones0243@proton.me>
2023-01-02 03:11:10 +0000

    karnak: copyright headers: update to SPDX license
    Change-Id: Ida613f7b112fe02033226f8c5e9d0d151e0b5a8e
    Signed-off-by: Kai Jones <kvjones0243@proton.me>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
