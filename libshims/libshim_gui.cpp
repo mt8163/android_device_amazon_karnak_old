a610b51363f2f7d98c6c73aff5e6b9d96997ed5a
R0rt1z2 <me@r0rt1z2.com>
2023-09-29 11:47:00 -0700

    karnak: libshims: Fix ABI issues
    Avoid passing negative values for outBytesPerStride and
    outBytesPerPixel as this can lead to crashes in some cases.
    Use nullptr and allow the updated method to manage the situation.
    
    Several constructor shims are implemented incorrectly because
    they completely ignore the fact that constructors have 'this-call'
    calling convention and should pass 'this ptr' as the first argument [1]
    as well as its return value, at least according ARM ABI [2].
    
    [1] https://review.lineageos.org/c/LineageOS/android_device_xiaomi_sdm710-common/+/360011/comments/eb6600df_3e2fb434
    [2] https://github.com/ARM-software/abi-aa/blob/617079d8a0d45bec83d351974849483cf0cc66d5/cppabi32/cppabi32.rst?plain=1#L576
    
    Co-authored-by: Ivan Vecera <ivan@cera.cz>
    
    Change-Id: I7f4e6f240c5a0f69fadbeb8a4209dcb4a96d24ea
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- libshims/libshim_gui.cpp
+++ libshims/libshim_gui.cpp
@@ -1,27 +1,27 @@
 #include <ui/GraphicBufferMapper.h>
 
-int32_t outBytesPerPixel = -1;
-int32_t outBytesPerStride = -1;
-
 namespace android {
 extern "C" status_t
 _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
-    buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr,
-    int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
+    void *thisptr, buffer_handle_t handle, uint32_t usage, const Rect &bounds,
+    void **vaddr, int32_t *outBytesPerPixel, int32_t *outBytesPerStride);
 
 extern "C" status_t
 _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
-    buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr) {
+    void *thisptr, buffer_handle_t handle, uint32_t usage, const Rect &bounds,
+    void **vaddr) {
   return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
-      handle, usage, bounds, vaddr, &outBytesPerPixel, &outBytesPerStride);
+      thisptr, handle, usage, bounds, vaddr, nullptr, nullptr);
 }
 
-extern "C" void _ZN7android13GraphicBuffer4lockEjPPvPiS3_(
-    uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel,
-    int32_t* outBytesPerStride);
+extern "C" void *_ZN7android13GraphicBuffer4lockEjPPvPiS3_(
+    void *thisptr, uint32_t inUsage, void **vaddr, int32_t *outBytesPerPixel,
+    int32_t *outBytesPerStride);
 
-extern "C" void _ZN7android13GraphicBuffer4lockEjPPv(uint32_t inUsage,
-                                                     void** vaddr) {
-  _ZN7android13GraphicBuffer4lockEjPPvPiS3_(inUsage, vaddr, &outBytesPerPixel, &outBytesPerStride);
+extern "C" void _ZN7android13GraphicBuffer4lockEjPPv(void *thisptr,
+                                                     uint32_t inUsage,
+                                                     void **vaddr) {
+  return _ZN7android13GraphicBuffer4lockEjPPvPiS3_(thisptr, inUsage, vaddr,
+                                                   nullptr, nullptr);
 }
-}  // namespace android
+} // namespace android
