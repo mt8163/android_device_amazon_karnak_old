276899e697d15df418c7eea3e589c65ace3506e9
Kai Jones <19639817+488315@users.noreply.github.com>
2023-10-09 03:20:28 -0700

    karnak: build `android.hardware.drm@1.1.vendor`
    fixes 10-09 09:00:17.173   622   622 F linker  : CANNOT LINK EXECUTABLE
    "/vendor/bin/hw/android.hardware.drm@1.1-service.widevine": library
    "android.hardware.drm@1.1.so" not found: needed by main executable
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -203,6 +203,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.4-service.clearkey \
     libdrm \
     libmockdrmcryptoplugin \
