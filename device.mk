9094729725fbe825c5d1d51c14f8e6bddbdf648d
Kai Jones <19639817+488315@users.noreply.github.com>
2023-10-09 22:31:21 -0700

    karnak: overlay: remove `overlay-lineage`
    this is not used in aosp
    
    Change-Id: I7aba247f6caa5d14ca6d42b0f13a460abfaeaaf3
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -16,8 +16,7 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Lights
 PRODUCT_PACKAGES +=\
