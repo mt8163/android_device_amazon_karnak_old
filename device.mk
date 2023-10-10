c92a5c4313df5071659f0639b57ee2eb4181744d
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-09 23:16:52 -0700

    karnak: wifi: Switch to Wifi service AIDL
    fixes:
    Cannot call isSupported because mWifiHal is null
    06:00:31.774                          WifiNative (573   671)  I    Vendor Hal not supported, ignoring start.
    06:00:31.774                              am_wtf (573   601)  I    [0,573,system_server,-1,WifiHal,Cannot call isSupported because mWifiHal is null]
    06:00:31.775                             WifiHal (573   671)  E    Cannot call isSupported because mWifiHal is null
    06:00:31.781                          WifiNative (573   671)  I    Vendor Hal not supported, ignoring createStaIface.
    06:00:31.781                              am_wtf (573   601)  I    [0,573,system_server,-1,WifiHal,Cannot call isSupported because mWifiHal is null]
    06:00:31.782                  WifiNl80211Manager (573   671)  D    Setting up interface for client mode: wlan0
    06:00:31.792                  WifiNl80211Manager (573   671)  E    Could not get IClientInterface instance from wificond
    06:00:31.793                          WifiNative (573   671)  E    Failed to setup iface in wificond=wlan0
    06:00:31.794                             WifiHal (573   671)  E    Cannot call isSupported because mWifiHal is null
    06:00:31.800                          WifiNative (573   671)  I    Vendor Hal not supported, ignoring removeStaIface.
    06:00:31.800                              am_wtf (573   601)  I    [0,573,system_server,-1,WifiHal,Cannot call isSupported because mWifiHal is null]
    
    Change-Id: Iaaf1a8ca202953cc40d926fa88f2afcfb313a2e0
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -57,7 +57,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     libwpa_client \
     hostapd \
     wpa_supplicant \
