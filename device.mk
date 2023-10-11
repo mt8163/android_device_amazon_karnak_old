5693474398b8e3ec71b517e1fe0c4800f0b9e84d
Kai Jones <19639817+488315@users.noreply.github.com>
2023-10-10 22:01:31 -0700

    karnak: graphics: switch `android.hardware.graphics.mapper` to `2.0`
    Change-Id: Ib06d929f5165728e3ab8b4c32c95111954f21477
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -109,7 +109,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-service \
     libion
 
