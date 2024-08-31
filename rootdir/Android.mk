3df314cc7e9a067d18942feba188bf8d923f688f
Kai Jones <19639817+488315@users.noreply.github.com>
2024-08-31 14:15:34 -0700

    karnak: rootdir: refactor
    - Imported rootdir from Fire OS 7 (karnak-user 9 PS7327.3336N 0026743736320 amz-p,release-keys).
    - Updated fstab configurations and init scripts based on the imported rootdir.

---
+++ rootdir/Android.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.mt8163.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_SRC_FILES := etc/init.mt8163.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.mt8163.usb.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_SRC_FILES := etc/init.mt8163.usb.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.mt8163
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
+LOCAL_SRC_FILES := etc/fstab.mt8163
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.mt8163.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_SRC_FILES    := etc/ueventd.mt8163.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
