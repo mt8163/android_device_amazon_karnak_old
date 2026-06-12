d01f7d02ad9321ff284227d4fcd2edf50ce68876
Kai Jones <kvjones0243@gmail.com>
2026-06-12 00:31:26 -0700

    karnak: extract: add mtkcam blob fixup
    Signed-off-by: Kai Jones <kvjones0243@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -68,6 +68,9 @@ function blob_fixup() {
         vendor/lib/libMtkOmxVdecEx.so)
             "${PATCHELF}" --add-needed "libshim_gui.so" "${2}"
             ;;
+        vendor/lib/libmtkcam_stdutils.so)
+            "${PATCHELF}" --add-needed "libshim_mtkcam.so" "${2}"
+            ;;
     esac
 }
 
