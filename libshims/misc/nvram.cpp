beafc2650bdffd1ff01e87a450c6aa06903fb4df
Kai Jones <kvjones0243@gmail.com>
2026-06-12 00:03:46 -0700

    karnak: libshims: shim old libbase startswith prototype
    Signed-off-by: Kai Jones <kvjones0243@gmail.com>

--- libshims/misc/nvram.cpp
+++ libshims/misc/nvram.cpp
@@ -1,1 +1,28 @@
-extern "C" void _ZN7android4base10StartsWithERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPKc() { }
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/strings.h>
+#include <string>
+
+namespace android {
+namespace base {
+
+bool StartsWith(const std::string& s, const char* prefix) {
+    return StartsWith(std::string_view(s), std::string_view(prefix));
+}
+
+}  // namespace base
+}  // namespace android
